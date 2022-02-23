d4c5c0d111c3c6315585c4ff9df610ed99182b87
mehmetalivargun <34211151+mehmetalivargun@users.noreply.github.com>
2022-02-23 12:48:10 +0300

    Initial Commit

---
+++ app/src/main/java/com/mehmetalivargun/countries/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.mehmetalivargun.countries.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(provideHttpLoggingInterceptor())
+            .build()
+    }
+
+
+}
